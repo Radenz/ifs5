114d34318ee2e110adf2ca58ca6b78a85045ef95
Radenz <radenrifqirahman@gmail.com>
2022-09-04 11:10:30 +0700

    fix: handle separator and chapter sub items

--- shared/transform.py
+++ shared/transform.py
@@ -6,11 +6,29 @@ import sys
 def transform(book):
     for i in range(len(book["sections"])):
         # Separator case
-        if type(book["sections"][i]) != dict:
+        if type(book["sections"][i]) != dict or "Chapter" not in book["sections"][i]:
             continue
         book["sections"][i]["Chapter"]["content"] = re.sub(
             r'\$(.*?)\$', r'\\\\(\1\\\\)', book["sections"][i]["Chapter"]["content"])
+
+        book["sections"][i]["Chapter"]["sub_items"] = transform_sub(
+            book["sections"][i]["Chapter"]["sub_items"]
+        )
+
     return book
+
+
+def transform_sub(sub):
+    for i in range(len(sub)):
+        if type(sub[i]) != dict or "Chapter" not in sub[i]:
+            continue
+        sub[i]["Chapter"]["content"] = re.sub(
+            r'\$(.*?)\$', r'\\\\(\1\\\\)', sub[i]["Chapter"]["content"])
+
+        sub[i]["Chapter"]["sub_items"] = transform_sub(
+            sub[i]["Chapter"]["sub_items"]
+        )
+    return sub
 
 
 if __name__ == '__main__':
@@ -19,5 +37,7 @@ if __name__ == '__main__':
             sys.exit(0)
 
     context, book = json.load(sys.stdin)
+    sys.stderr.write(json.dumps(book, indent=2))
     book = transform(book)
+    sys.stderr.write(json.dumps(book, indent=2))
     print(json.dumps(book))
